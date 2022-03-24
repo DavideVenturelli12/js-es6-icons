255326c1d6ba3dbc48939f261a3e681a2f51fa0d
Davide Venturelli <dventurelli90@gmail.com>
2022-03-24 09:18:17 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -135,30 +135,76 @@ const arrayIcone = [
 console.table(arrayIcone);
 
 //Milestone 1
-//Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è 
+//Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è
 //presente il nome dell'icona e l'icona stessa.
 
-const containerIcone = document.querySelector('.icons-container');
-
-arrayIcone.forEach((elemento) => {
-    elementoCreato(elemento);
-})
-
-
-function elementoCreato(arrayIcone) {
-
-    const cellaElemento = document.createElement('div');
-    cellaElemento.classList.add('col-2', 'text-center', 'cella',);
-
-
-    cellaElemento.innerHTML = `
-    <i class="${arrayIcone.family} ${arrayIcone.prefix}${arrayIcone.name}"></i>
-    <p>${arrayIcone.name}</p>`
-
-    containerIcone.appendChild(cellaElemento);
-}
 
 //Milestone 2
 //Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone
 //del colore corrispondente.
 
+//recupero l'icons-container dall'HTML
+const containerIcone = document.querySelector('.icons-container');
+
+//eseguo un ciclo forEach per ciclare gli object nell'array
+arrayIcone.forEach((elemento) => {
+    elementoCreato(elemento);
+})
+
+//per ogni object eseguo una funzione che crea uan cella
+function elementoCreato(arrayIcone) {
+
+    const cellaElemento = document.createElement('div');
+    cellaElemento.classList.add('col-2', 'text-center', 'border', 'py-3', 'cella',);
+
+    //inserisco all'interno della cella le proprietà dell'object
+    cellaElemento.innerHTML = `
+    <i class="${arrayIcone.family} ${arrayIcone.prefix}${arrayIcone.name} ${arrayIcone.color} "></i>
+    <p>${arrayIcone.name}</p>`
+
+    //inserisco la cella creata nell'HTML
+    containerIcone.appendChild(cellaElemento);
+}
+
+//Milestone 3
+//Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone
+//(animal, vegetable, user).Quando l'utente seleziona un tipo dalla select, visualizzare solamente 
+//le icone corrispondenti.
+
+//recupero il select dall'HTML
+const select = document.getElementById('select-icons');
+
+//aggiungio un evento al select
+select.addEventListener('change', (event) => {
+    //svuoto prima di tutto il conteainer di icone
+    containerIcone.innerHTML = '';
+
+    //eseguo dei cicli if a seconda del type dell'object 
+    //e inserendolo nell'HTML
+    if (event.target.value === 'animals') { //preso dal value nell'HTML
+        arrayIcone.filter((elemento) => {
+
+            if (elemento.type === 'animal') { //preso dal type nell'object
+                elementoCreato(elemento);
+            }
+        })
+    } else if (event.target.value === 'vegetables') { //preso dal value nell'HTML
+        arrayIcone.filter((elemento) => {
+
+            if (elemento.type === 'vegetable') { //preso dal type nell'object
+                elementoCreato(elemento);
+            }
+        })
+    } else if (event.target.value === 'user') { //preso dal value nell'HTML
+        arrayIcone.filter((elemento) => {
+
+            if (elemento.type === 'user') { //preso dal type nell'object
+                elementoCreato(elemento);
+            }
+        })
+    } else if (event.target.value === 'all') { //preso dal value nell'HTML
+        arrayIcone.forEach((elemento) => {
+            elementoCreato(elemento);
+        })
+    }
+})
